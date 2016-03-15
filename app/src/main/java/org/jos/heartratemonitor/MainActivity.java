b897542ee321ba7c84ca48832a7df8585656ec12
josmas <josmasflores@gmail.com>
2016-03-15 13:54:09 +0000

    scan for devices and log them

--- app/src/main/java/org/jos/heartratemonitor/MainActivity.java
+++ app/src/main/java/org/jos/heartratemonitor/MainActivity.java
@@ -1,26 +1,35 @@
 package org.jos.heartratemonitor;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
   private static final int REQUEST_ENABLE_BT = 1;
+  private static final long SCAN_PERIOD = 20000;
   private BluetoothAdapter mBluetoothAdapter;
+  private List mLeDevices = new ArrayList<BluetoothDevice>();
+  private boolean isScanning;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -40,20 +49,9 @@ public class MainActivity extends AppCompatActivity {
 
     if (bleSupport()){
       ((TextView)findViewById(R.id.state)).setText(R.string.ble_available);
-      // Initializes Bluetooth adapter.
-      final BluetoothManager bluetoothManager =
-          (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-        mBluetoothAdapter = bluetoothManager.getAdapter();
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-          Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-          startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-      }
-      else {
-        ((TextView)findViewById(R.id.state)).setText(R.string.ble_not_supported);
+      bleEnabled();
+      if (mBluetoothAdapter.isEnabled()){
+        startScanning();
       }
     }
     else {
@@ -67,6 +65,68 @@ public class MainActivity extends AppCompatActivity {
       return false;
     }
     return true;
+  }
+
+  private void bleEnabled() {
+    // Initializes Bluetooth adapter.
+    final BluetoothManager bluetoothManager =
+        (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+      mBluetoothAdapter = bluetoothManager.getAdapter();
+      // Ensures Bluetooth is available on the device and it is enabled. If not,
+      // displays a dialog requesting user permission to enable Bluetooth.
+      if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+      }
+    }
+    else {
+      ((TextView)findViewById(R.id.state)).setText(R.string.ble_not_supported);
+    }
+  }
+
+  public void startScanning() {
+    Handler mHandler = new Handler();
+    if (!mLeDevices.isEmpty()) {
+      mLeDevices.clear();
+    }
+    // Stops scanning after a pre-defined scan period.
+    mHandler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        isScanning = false;
+        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        ((TextView)findViewById(R.id.state)).setText("Stopped Scanning");
+      }
+    }, SCAN_PERIOD);
+    mBluetoothAdapter.startLeScan(mLeScanCallback);
+  }
+
+  public void stopScanning() {
+    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+  }
+
+  private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+    @Override
+    public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+      runOnUiThread(new Runnable() {
+        @Override
+        public void run() {
+          isScanning = true;
+          addDevice(device, rssi, scanRecord);
+          Log.i("SCANNER", "Got a result: " + device.getName() + " " + rssi + " " + scanRecord.toString());
+        }
+      });
+    }
+  };
+
+  // RSSI is the strength of the signal; can be used to approximate distance
+  // (not accurate on Android). I won't be using it for now
+  private void addDevice(BluetoothDevice device, int rssi, byte[] scanRecord) {
+    if (!mLeDevices.contains(device)) {
+      mLeDevices.add(device);
+    }
   }
 
   //TODO (jos) Do I need to figure out if the BLE changes state?
