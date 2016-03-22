4ea8bcc16f9ae56a2aae74e9ca99e53fbd7ba446
josmas <josmasflores@gmail.com>
2016-03-22 19:25:06 +0000

    cleaning up GATT attributes

--- app/src/main/java/org/jos/heartratemonitor/SampleGattAttributes.java
+++ app/src/main/java/org/jos/heartratemonitor/SampleGattAttributes.java
@@ -24,18 +24,20 @@ import java.util.HashMap;
 public class SampleGattAttributes {
   private static HashMap<String, String> attributes = new HashMap();
   public final static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
-  public final static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+  public final static String HEART_RATE_SERVICE = "0000180d-0000-1000-8000-00805f9b34fb";
+  public final static String DEVICE_INFORMATION_SERVICE = "0000180a-0000-1000-8000-00805f9b34fb";
+  public final static String MANUFACTURER_NAME_STRING = "00002a29-0000-1000-8000-00805f9b34fb";
   public final static String BATTERY_LEVEL_SER = "0000180f-0000-1000-8000-00805f9b34fb";
   public final static String BATTERY_LEVEL_CHAR = "00002a19-0000-1000-8000-00805f9b34fb";
 
   static {
     // Sample Services.
-    attributes.put("0000180d-0000-1000-8000-00805f9b34fb", "Heart Rate Service");
-    attributes.put("0000180a-0000-1000-8000-00805f9b34fb", "Device Information Service");
+    attributes.put(HEART_RATE_SERVICE, "Heart Rate Service");
+    attributes.put(DEVICE_INFORMATION_SERVICE, "Device Information Service");
     attributes.put(BATTERY_LEVEL_SER, "Battery Level Service");
     // Sample Characteristics.
     attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
-    attributes.put("00002a29-0000-1000-8000-00805f9b34fb", "Manufacturer Name String");
+    attributes.put(MANUFACTURER_NAME_STRING, "Manufacturer Name String");
     attributes.put(BATTERY_LEVEL_CHAR, "Battery Level Measurement");
 
   }
