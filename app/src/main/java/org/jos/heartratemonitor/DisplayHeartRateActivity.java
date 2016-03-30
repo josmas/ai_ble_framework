345f6068e23ed4e7754d0368e5f2c57cc4bb0560
josmas <josmasflores@gmail.com>
2016-03-30 16:29:36 +0100

    clean up on displayServices for heart rate monitor activity

--- app/src/main/java/org/jos/heartratemonitor/DisplayHeartRateActivity.java
+++ app/src/main/java/org/jos/heartratemonitor/DisplayHeartRateActivity.java
@@ -4,7 +4,6 @@ import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.os.Bundle;
 import android.util.Log;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
@@ -15,10 +14,28 @@ import java.util.UUID;
  */
 public class DisplayHeartRateActivity extends MainActivity {
 
-  //TODO (jos) move here all the constants from SampleGattAttributes that are needed on this Activity only.
-  private final String LIST_NAME = "NAME";
-  private final String LIST_UUID = "UUID";
-  private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics = new ArrayList<>();
+  public final static String HEART_RATE_SERVICE = "0000180d-0000-1000-8000-00805f9b34fb";
+  public final static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb";
+  public final static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+  public final static String BATTERY_LEVEL_SER = "0000180f-0000-1000-8000-00805f9b34fb";
+  public final static String BATTERY_LEVEL_CHAR = "00002a19-0000-1000-8000-00805f9b34fb";
+  public final static String CLIENT_CHARACTERISTIC_CONFIG_BATTERY = "00002908-0000-1000-8000-00805f9b34fb";
+  private static HashMap<String, String> attributes = new HashMap<>();
+
+  static {
+    // Sample Services.
+    attributes.put(HEART_RATE_SERVICE, "Heart Rate Service");
+    attributes.put(BATTERY_LEVEL_SER, "Battery Level Service");
+    // Sample Characteristics.
+    attributes.put(HEART_RATE_MEASUREMENT, "Heart Rate Measurement");
+    attributes.put(BATTERY_LEVEL_CHAR, "Battery Level Measurement");
+  }
+
+  public static String lookup(String uuid, String defaultName) {
+    String name = attributes.get(uuid);
+    return name == null ? defaultName : name;
+  }
+
   private BluetoothGattCharacteristic mNotifyCharacteristic;
 
   @Override
@@ -27,16 +44,12 @@ public class DisplayHeartRateActivity extends MainActivity {
   }
 
   /**
-   * TODO (jos) This method could return a list of lists with a Service and its Characteristics in
-   * each row. I need the characteristics to either read from or set notifications in the mService.
-   * So this method should filter only the Services we want, and add them to a list (of lists).
-   * TODO (jos) in fact, this method could be placed in the main class and pass a list of services
-   * here as a filter... let's see how it goes after creating the one for RFduino.
-   * //TODO (jos) after having created the one for RFDuino, this could go two ways; either make it
-   * generic and use a filter, or make it specific and only for the extension/device at hand.
+   * Discovering Services and Characteristics. For now, setting up Notification for hear rate, but
+   * the battery service could also be used. The monitor has also additional services.
    * @param supportedGattServices
    */
   @Override
+  //TODO (jos) It is more of a setupServicesAndCharacteristics than a displayServices
   public void displayServices(List<BluetoothGattService> supportedGattServices) {
     Log.i("BLE", "All Services are: " + supportedGattServices);
 
@@ -44,63 +57,46 @@ public class DisplayHeartRateActivity extends MainActivity {
     String uuid;
     String unknownServiceString = getResources().getString(R.string.unknown_service);
     String unknownCharaString = getResources().getString(R.string.unknown_characteristic);
-    ArrayList<HashMap<String, String>> gattServiceData = new ArrayList<>();
-    ArrayList<ArrayList<HashMap<String, String>>> gattCharacteristicData = new ArrayList<>();
-    mGattCharacteristics = new ArrayList<>();
 
     for (BluetoothGattService gattService : supportedGattServices) {
-      HashMap<String, String> currentServiceData = new HashMap<>();
       uuid = gattService.getUuid().toString();
-      String serviceFound = SampleGattAttributes.lookup(uuid, unknownServiceString);
+      String serviceFound = lookup(uuid, unknownServiceString);
       if (serviceFound.equals(unknownServiceString)) continue;
-      currentServiceData.put(LIST_NAME, serviceFound);
-      currentServiceData.put(LIST_UUID, uuid);
-      gattServiceData.add(currentServiceData);
       Log.i("BLE", "\n\n-------------------------------------------------------------");
-      Log.i("BLE", "Service Data for heart rate: " + currentServiceData);
-
-      ArrayList<HashMap<String, String>> gattCharacteristicGroupData = new ArrayList<>();
-      List<BluetoothGattCharacteristic> gattCharacteristics = gattService.getCharacteristics();
-      ArrayList<BluetoothGattCharacteristic> charas = new ArrayList<>();
+      Log.i("BLE", "Service Data for Heart Rate: " + serviceFound);
 
       // Loops through available Characteristics.
-      for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
+      for (BluetoothGattCharacteristic gattCharacteristic : gattService.getCharacteristics()) {
         uuid = gattCharacteristic.getUuid().toString();
-        String characteristicFound = SampleGattAttributes.lookup(uuid, unknownCharaString);
+        String characteristicFound = lookup(uuid, unknownCharaString);
         if (characteristicFound.equals(unknownCharaString)) continue; // Skip if not interested.
-        Log.i("BLE", "Characteristic for heart rate: " + gattCharacteristic);
-        charas.add(gattCharacteristic);
-        HashMap<String, String> currentCharaData = new HashMap<>();
-        currentCharaData.put(LIST_NAME, characteristicFound);
-        currentCharaData.put(LIST_UUID, uuid);
-        gattCharacteristicGroupData.add(currentCharaData);
-        Log.i("BLE", "Characteristic Data for heart rate: " + currentCharaData);
-        // TODO (jos) In theory, this methods ends here, with a list of lists. The read and notification
-        // settings should be done in a separate method.
+        Log.i("BLE", "Characteristic for Heart Rate monitor: " + characteristicFound);
 
         // Tried to read the value from gattCharacteristic here, but seems like it's null at this
         // stage. It needs to be read in the service (on callback).
         // Read and Notify for the heart rate measure; this should be its own method.
-        if (UUID.fromString(SampleGattAttributes.HEART_RATE_MEASUREMENT).equals(gattCharacteristic.getUuid())) {
+        //TODO (jos) for now just setting a Notification in the Heart Rate Meassure.
+        // Could also provide a method to read from the battery level characteristic.
+        if (UUID.fromString(HEART_RATE_MEASUREMENT).equals(gattCharacteristic.getUuid())) {
 
           final int charaProp = gattCharacteristic.getProperties();
           if ((charaProp | BluetoothGattCharacteristic.PROPERTY_READ) > 0) {
             // If there is an active notification on a characteristic, clear
             // it first so it doesn't update the data field on the user interface.
             if (mNotifyCharacteristic != null) {
-              bluetoothLeService.setCharacteristicNotification(mNotifyCharacteristic, false);
+              bluetoothLeService.setCharacteristicNotification(mNotifyCharacteristic, false,
+                  gattCharacteristic.getUuid(), UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
               mNotifyCharacteristic = null;
             }
             bluetoothLeService.readCharacteristic(gattCharacteristic);
           }
           if ((charaProp | BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0) {
             mNotifyCharacteristic = gattCharacteristic;
-            bluetoothLeService.setCharacteristicNotification(gattCharacteristic, true);
+            bluetoothLeService.setCharacteristicNotification(gattCharacteristic, true,
+                gattCharacteristic.getUuid(), UUID.fromString(CLIENT_CHARACTERISTIC_CONFIG));
           }
         }
       }
-      mGattCharacteristics.add(charas);
-      gattCharacteristicData.add(gattCharacteristicGroupData);
     }
 
   }
